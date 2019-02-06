33571dfd2a2dda27deca73ccfafdded317e941c0
Cosmo DiGiulio <cosmo.digiulio@gmail.com>
2019-02-06 19:22:41 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,6 +3,11 @@ def levitation_quiz
 	  puts "What is the spell that enacts levitation?"
 	  answer gets.chomp
 	  break if answer == "Wingardium Leviosa"
+	end
+	puts "You Passed the quiz!"
 end
+end
+
+leviation_quiz
 
 
